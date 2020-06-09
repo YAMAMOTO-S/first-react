19780dc218f0a2a9a72cf2bdfb7d3379bc336043
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-06-09 21:28:20 +0900

    props, defaultProps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 function App() {
+
+  const profiles = [
+    { name: "Taro", age: 23},
+    {name: "hanako", age: 10},
+    {age: 10}
+  ]
+
   return (
-    <h1 className="App">
-      hellow
-    </h1>
+    <div className="App">
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
   );
+}
+
+const User = (props) => {
+return <div>Hi, I am {props.name}, age is {props.age}</div>
+}
+
+User.defaultProps = {
+  age: 1,
+  name: "名無し"
 }
 
 export default App;
