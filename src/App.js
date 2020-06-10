30d8bf58bf7cba53e82c9292f14dfd949add2b57
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-06-10 13:33:06 +0900

    redux action

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+// import React, { Component } from 'react';
 
-function App() {
 
-  const profiles = [
-    { name: "Taro", age: 23},
-    {name: "hanako", age: 10},
-    {name: "aaaa", age: 3}
-  ]
+// function App() {
+//   construnctor(props) {
+//     super(props)
+//     this.state = { count: 0 }
+//   }
 
-  return (
-    <div className="App">
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       <Counter></Counter>
+//     </div>
+//   );
+// }
+
+// class Counter extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <div>count: { this.state.count }</div>
+//         <button>+1</button>
+//         <button>-1</button>
+//       </React.Fragment>
+//     )
+//   } 
+// }
+
+// export default App;
+import React, { Component } from 'react';
+
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
-
-const User = (props) => {
-return <div>Hi, I am {props.name}, age is {props.age}</div>
-}
-
-//Userの受け付けるpropsの型を設定するよ
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
-
 export default App;
---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+// javascript のオブジェクトのこと
+// タイプはユニークでなければナラニア
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT =  'DECREMENT'
+
+export const increment = () => {
+  return {
+    type: INCREMENT
+  }
+}
+
+export const decrement = () => {
+  return {
+    type: DECREMENT
+  }
+} 
